87c514fbaba4942e90d4a64b0ebf9f5397bfe126
Juan D. Pecho <jdelacruzp@uni.pe>
2024-07-21 20:38:00 -0500

    removing unnncesesary UserRepository

--- bookingapi/src/main/java/com/juandiegodelacruz/bookingapi/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.juandiegodelacruz.bookingapi.repository;
-import com.juandiegodelacruz.bookingapi.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Book, Long> {
-}
